ecca00c29a9a79bbca3569757e265818a85a8f7b
Abhrajit Ghosh <abhrajitghosh117@gmail.com>
2021-10-23 14:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ let songs = [
     {songName: "NCS TRACK", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
     {songName: "The Nights - Avicii", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
     {songName: "Bulave Tujhe Yaar aj Meri Galiya", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Scars to your beautiful - Alessia Cara", filePath: "songs/10.mp3", coverPath: "covers/10.png"},
-    {songName: "Khaab - Parmish Verma", filePath: "songs/11.mp3", coverPath: "covers/11.jpg"},
+    {songName: "Scars to your beautiful - Alessia Cara", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Khaab - Parmish Verma", filePath: "songs/11.mp3", coverPath: "covers/11.jpeg"},
 ]
 
 songItems.forEach((element, i)=>{ 
